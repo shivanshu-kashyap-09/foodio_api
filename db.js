1cf0b7e0de2311254d797d6d7e78c9f7d3e889b3
shivanshu-kashyap-09 <kashyapshivanshu63@gmail.com>
2025-12-12 23:13:17 +0530

    update-db-and-env

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const fs = require('fs');
 
 const connection = mysql.createConnection({
-  host:  process.env.HOST_NAME,
+  host: process.env.HOST_NAME,
   user: process.env.USER,
   password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.PORT 
-})
+  multipleStatements: true
+});
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting: ' + err.stack);
+    console.error("Connection error:", err);
     return;
   }
+  console.log("Connected to MySQL server");
   console.log('Connected as ID ' + connection.threadId);
+  const sqlFile = fs.readFileSync("foodio.sql", "utf8");
+
+  connection.query(sqlFile, (err) => {
+    if (err) {
+      console.error("Error executing SQL file:", err);
+    } else {
+      console.log("Database schema imported successfully");
+    }
+  });
 });
 
 module.exports = connection;
